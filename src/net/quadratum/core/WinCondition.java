18619f972f890355c3b73781faa8771759d860f2
amfr <amfr@amfr.org>
2011-04-23 17:03:01 -0400

    Added more to GameCore, changed some internal classes and interfaces

--- src/net/quadratum/core/WinCondition.java
+++ src/net/quadratum/core/WinCondition.java
@@ -7,16 +7,16 @@ public interface WinCondition {
 	/**
 	 * Determines if a player has won the game.
 	 * @param units The map of points to unit IDs for this player.
-	 * @param player The player who we are checking.
+	 * @param playerInformation The information of the player who we are checking.
 	 * @return true if the player has won, false otherwise.
 	 */
-	boolean hasPlayerWon(Map<Point,Integer> units, Player player);
+	boolean hasPlayerWon(Map<Point,Integer> units, PlayerInformation playerInformation);
 	
 	/**
 	 * Determines if a player has lost the game.
 	 * @param units The map of points to unit IDs for this player.
-	 * @param player The player who we are checking.
+	 * @param playerInformation The information of player who we are checking.
 	 * @return true if the player has lost, false otherwise.
 	 */
-    boolean eliminatePlayer(Map<Point,Integer> units, Player player);
+    boolean eliminatePlayer(Map<Point,Integer> units, PlayerInformation playerInformation);
 }
