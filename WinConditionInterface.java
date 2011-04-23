47965e2d66a50947b02a4dd6163c3043b819a29a
Zircean <Zircean@localhost>
2011-04-23 14:56:37 -0400

    -Removed the interfaces floating around in the root folder.

--- WinConditionInterface.java
+++
@@ -1,5 +0,0 @@
-interface WinConditionInterface
-{
-	boolean hasPlayerWon(Map<Point, int> units, Player player); // Has this player won?
-	boolean eliminatePlayer(Map<Point, int> units, Player player); // Has this player lost?
-}
