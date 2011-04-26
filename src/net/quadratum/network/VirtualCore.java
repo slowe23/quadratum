aeaaf5838c4eb4c5be6dc89137dea9423624263d
Zircean <Zircean@localhost>
2011-04-26 17:59:43 -0400

    +Adding more network functionality

--- src/net/quadratum/network/VirtualCore.java
+++ src/net/quadratum/network/VirtualCore.java
@@ -6,15 +6,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.quadratum.core.Action.ActionType;
 import net.quadratum.core.Core;
 import net.quadratum.core.Player;
 import net.quadratum.core.MapPoint;
+import net.quadratum.core.PlayerInformation;
 import net.quadratum.core.Unit;
 
-public class VirtualCore implements Core {
+public class VirtualCore extends Thread implements Core {
 	
 	/** Socket that connects to the actual player. */
 	Socket _sockToServer;
@@ -22,6 +26,19 @@ public class VirtualCore implements Core {
 	BufferedWriter _out;
 	/** Buffered reader from the player. */
 	BufferedReader _in;
+	
+	/** Local player. */
+	Player _localPlayer;
+	/** Local player's information. */
+	PlayerInformation _localInfo;
+	
+	/** Nonlocal players. */
+	List<VirtualPlayer> _virtualPlayers;
+	/** Nonlocal player information. */
+	List<PlayerInformation> _virtualInfo;
+	
+	/** Used for storage. */
+	Map<String,String> _responses;
 	
 	public VirtualCore(Socket sock) {
 		_sockToServer = sock;
@@ -32,11 +49,21 @@ public class VirtualCore implements Core {
 			e.printStackTrace();
 		}
 		
+		_responses = Collections.synchronizedMap(new HashMap<String,String>());		
 	}
 
 	@Override
 	public void addPlayer(Player p, String playerName, int maxUnits) {
-		// TODO Auto-generated method stub
+		if (p instanceof VirtualPlayer) {
+			// A VirtualPlayer represents somebody on the actual game, but over the
+			// network.
+			_virtualPlayers.add((VirtualPlayer) p);
+			_virtualInfo.add(new PlayerInformation(playerName, maxUnits));
+		} else {
+			// A VirtualCore should only have one local player associated.
+			_localPlayer = p;
+			_localInfo = new PlayerInformation(playerName, maxUnits);
+		}
 		
 	}
 
@@ -70,8 +97,15 @@ public class VirtualCore implements Core {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// TODO get return val
-		return false;
+		// protocol: <unitaction \t> success
+		String[] s = getResponse("unitaction");
+		boolean b = false;
+		try {
+			b = Boolean.parseBoolean(s[0]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return b;
 	}
 
 	@Override
@@ -105,8 +139,15 @@ public class VirtualCore implements Core {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// TODO get return val
-		return false;
+		// protocol: <unitplaced \t> success
+		String[] s = getResponse("unitplaced");
+		boolean b = false;
+		try {
+			b = Boolean.parseBoolean(s[0]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return b;
 	}
 
 	@Override
@@ -117,8 +158,15 @@ public class VirtualCore implements Core {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// TODO get return val
-		return 0;
+		// protocol: <remainingunits \t> remunits
+		String[] s = getResponse("remainingunits");
+		int i = 0;
+		try {
+			i = Integer.parseInt(s[0]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return i;
 	}
 
 	@Override
@@ -140,8 +188,15 @@ public class VirtualCore implements Core {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// TODO get return val
-		return false;
+		// protocol: <unitupdated \t> success
+		String[] s = getResponse("unitupdated");
+		boolean b = false;
+		try {
+			b = Boolean.parseBoolean(s[0]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return b;
 	}
 	
 	@Override
@@ -152,8 +207,17 @@ public class VirtualCore implements Core {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// TODO get return val
-		return null;
+		// protocol: <playername \t> id \t name
+		String[] s = getResponse("playername");
+		/*
+		int id = 0;
+		try {
+			id = Integer.parseInt(s[0]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		*/
+		return s[1];
 	}
 
 	@Override
@@ -163,8 +227,93 @@ public class VirtualCore implements Core {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// TODO get return val
-		return 0;
+		// protocol: <resources \t> res
+		String[] s = getResponse("resources");
+		int i = 0;
+		try {
+			i = Integer.parseInt(s[0]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+	
+	private String[] getResponse(String resp) {
+		String[] s = null;
+		boolean done = false;
+		while (!done) {
+			synchronized (_responses) {
+				done = _responses.containsKey(resp);
+				if (done) {
+					s = _responses.remove(resp).split("\t");
+				}
+			}
+		}
+		return s;
+	}
+	
+	private void process(String message) {
+		String[] parts = message.split("\t");
+		// Look at the first part of the incoming message
+		if (parts[0].equals("start")) {
+			// The game has started.
+			// TODO protocol
+		} else if (parts[0].equals("updatepieces")) {
+			// The game is telling the player what pieces they are
+			// allowed to have.
+			// TODO protocol
+		} else if (parts[0].equals("end")) {
+			// The game has ended.
+			// TODO get stats
+			_localPlayer.end(null);
+		} else if (parts[0].equals("lost")) {
+			// This player has just lost.
+			_localPlayer.lost();
+		} else if (parts[0].equals("turnstart")) {
+			// It is now this player's turn.
+			_localPlayer.turnStart();
+		} else if (parts[0].equals("updatemapdata")) {
+			// The host is sending new map data.
+			// TODO protocol
+		} else if (parts[0].equals("updatemap")) {
+			// The host is sending new information about units and actions.
+			// TODO protocol
+		} else if (parts[0].equals("chat")) {
+			// A chat message was sent.
+			// protocol: chat \t id \t message
+			try {
+				int id = Integer.parseInt(parts[1]);
+				_localPlayer.chatMessage(id, parts[2]);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		} else {
+			// A response was sent. Put it in the response map.
+			String resp = "";
+			for (int i = 1; i < parts.length; i++) {
+				resp += parts[i];
+				if (i < parts.length - 1) {
+					resp += "\t";
+				}
+			}
+			_responses.put(parts[0],resp);
+		}
+	}
+	
+	/**
+	 * Runs this VirtualCore.
+	 */
+	public void run() {
+		boolean threadRunning = true;
+		
+		String line;
+		try {
+			while (threadRunning && (line = _in.readLine()) != null) {
+				process(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
