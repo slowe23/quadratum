347166057284767e5602dd6832664b1ecaacb84d
amfr <amfr@amfr.org>
2011-04-26 19:30:32 -0400

    Added placeUnit to GameCore and updated its signature in the interface

--- src/net/quadratum/core/UnitInformation.java
+++ src/net/quadratum/core/UnitInformation.java
@@ -8,4 +8,15 @@ public class UnitInformation
 	boolean _hasAttacked;
 	/** The position of the unit. */
 	MapPoint _position;
+	
+	/**
+	 * Constructor for UnitInformation.
+	 * @param position the position of the unit
+	 */
+	public UnitInformation(MapPoint position)
+	{
+		_position = new MapPoint(position);
+		_hasMoved = false;
+		_hasAttacked = false;
+	}
 }
