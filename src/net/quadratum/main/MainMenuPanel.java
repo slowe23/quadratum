960e3a3af6b7affdd65e2ca737c7ca80f61c736b
Zircean <Zircean@localhost>
2011-04-27 15:37:36 -0400

    *Fixing my stuff to use changed interfaces.

---
+++ src/net/quadratum/main/MainMenuPanel.java
@@ -0,0 +1,81 @@
+package net.quadratum.main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class MainMenuPanel extends JPanel{
+
+	private final String _title = "Main Menu";
+	private JButton _singlePlayerBtn, _multiPlayerBtn, _loadBtn, _quitBtn;
+	//private JButton _helpBtn;
+	
+	
+	public MainMenuPanel(ActionListener al) {
+		super(new BorderLayout());
+		setSize(MainConstants.DEFAULT_WINDOW_W, MainConstants.DEFAULT_WINDOW_H);
+		
+		JPanel holder = new JPanel();
+		
+		// Use vertical layout for buttons
+		holder.setLayout(new BoxLayout(holder, BoxLayout.Y_AXIS));
+		
+		//Title
+		JLabel title = new JLabel(_title);
+		title.setAlignmentX(Component.CENTER_ALIGNMENT);
+		holder.add(title);
+		
+		// Create buttons
+		_singlePlayerBtn = new JButton("New game - single player");
+		_multiPlayerBtn = new JButton("New network game - multiplayer");
+		_loadBtn = new JButton("Load saved game");
+		_quitBtn = new JButton("Quit");
+		//_helpBtn = new JButton("Help");
+		
+		// Initialize buttons
+		_singlePlayerBtn.setActionCommand(MainConstants.SINGLE);
+		_singlePlayerBtn.addActionListener(al);
+		_singlePlayerBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+		_multiPlayerBtn.setActionCommand(MainConstants.NETWORK);
+		_multiPlayerBtn.addActionListener(al);
+		_multiPlayerBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+		_loadBtn.setActionCommand(MainConstants.LOAD);
+		_loadBtn.addActionListener(al);
+		_loadBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+		_quitBtn.setActionCommand(MainConstants.QUIT);
+		_quitBtn.addActionListener(al);
+		_quitBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+		//_helpBtn.setActionCommand("help");
+		//_helpBtn.addActionListener(al);
+		
+		// sizing prefs
+		_singlePlayerBtn.setPreferredSize(MainConstants.BUTTON_DIM);
+		_multiPlayerBtn.setPreferredSize(MainConstants.BUTTON_DIM);
+		_loadBtn.setPreferredSize(MainConstants.BUTTON_DIM);
+		_quitBtn.setPreferredSize(MainConstants.BUTTON_DIM);
+		
+		// Add buttons
+		holder.add(Box.createVerticalGlue());
+		holder.add(_singlePlayerBtn);
+		holder.add(Box.createRigidArea(new Dimension(0,10)));
+		holder.add(_multiPlayerBtn);
+		holder.add(Box.createRigidArea(new Dimension(0,10)));
+		holder.add(_loadBtn);
+		holder.add(Box.createRigidArea(new Dimension(0,10)));
+		//add(_helpBtn);
+		//holder.add(Box.createRigidArea(new Dimension(0,5)));
+		holder.add(_quitBtn);
+		holder.add(Box.createVerticalGlue());
+		holder.add(Box.createRigidArea(new Dimension(0,50)));
+	
+		add(holder, BorderLayout.CENTER);
+		//holder.setAlignmentY(CENTER_ALIGNMENT);
+		
+//		// By default, do not go visible from within
+//		setEnabled(false);
+//		setVisible(false);
+	}
+	
+
+}
