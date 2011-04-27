4e903b310891652d8b5cc07a70733123b37012fa
amfr <amfr@amfr.org>
2011-04-27 19:19:00 -0400

    Minor cleaning up

---
+++ src/net/quadratum/main/test/MainTest.java
@@ -0,0 +1,25 @@
+package net.quadratum.main.test;
+
+import net.quadratum.gui.*;
+import net.quadratum.core.*;
+import net.quadratum.ai.test.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class MainTest implements Main {
+	
+	public static void main(String[] args)
+	{
+		GUIPlayer player = new GUIPlayer();
+		TestAI_MTC ai = new TestAI_MTC();
+		GameCore core = new GameCore(new MainTest(), "null", new CheckWinner(), new ArrayList<Piece>());
+		core.addPlayer(ai, "AI Player", 10);
+		core.addPlayer(player, "Test Player", 10);
+		core.start();
+	}
+
+	@Override
+	public void returnControl() {
+		System.out.println("Done.");
+	}
+}
