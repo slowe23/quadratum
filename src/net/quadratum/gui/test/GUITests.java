01552527b15a78d72f2df14f2d8a83335c181029
Zircean <Zircean@localhost>
2011-04-28 20:38:05 -0400

    *Fixed IllegalStateException in TestAI.

---
+++ src/net/quadratum/gui/test/GUITests.java
@@ -0,0 +1,14 @@
+package net.quadratum.gui.test;
+
+import net.quadratum.core.Core;
+import net.quadratum.gui.GUIPlayer;
+
+public class GUITests {
+
+	public static void main(String[] args) {
+		GUIPlayer pl = new GUIPlayer();
+		Core c = new TestingCore();
+		c.addPlayer(pl, "Test Player", 100);
+		c.startGame();
+	}
+}
---
+++ src/net/quadratum/main/test/MainTest.java
@@ -0,0 +1,27 @@
+package net.quadratum.main.test;
+
+import java.util.ArrayList;
+
+import net.quadratum.ai.test.TestAI_MTC;
+import net.quadratum.core.GameCore;
+import net.quadratum.core.Piece;
+import net.quadratum.gui.GUIPlayer;
+import net.quadratum.main.Main;
+
+public class MainTest implements Main {
+	
+	public static void main(String[] args)
+	{
+		GUIPlayer player = new GUIPlayer();
+		TestAI_MTC ai = new TestAI_MTC();
+		GameCore core = new GameCore(new MainTest(), "null", new CheckWinnerTest(), new ArrayList<Piece>());
+		core.addPlayer(ai, "AI Player", 10);
+		core.addPlayer(player, "Test Player", 10);
+		core.startGame();
+	}
+
+	@Override
+	public void returnControl() {
+		System.out.println("Done.");
+	}
+}
