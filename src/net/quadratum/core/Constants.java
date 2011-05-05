6b59d23c78c31953b23fb9e225f0fe558cb3ed7f
amfr <amfr@amfr.org>
2011-05-05 17:36:55 -0400

    Fixed TestingCore

---
+++ src/net/quadratum/core/Constants.java
@@ -0,0 +1,28 @@
+package net.quadratum.core;
+
+public class Constants
+{
+	/** Maximum players a game can have. */
+	public static final int MAX_PLAYERS = 8;
+	/** Internal size of a unit. */
+	public static final int UNIT_SIZE = 8;
+	/** Resources gained per turn per unit on a resource square. */
+	public static final int RESOURCES_PER_TURN = 10;
+	/** Initial move radius of units. */
+	public static final int INITIAL_MOVE = 0;
+	/** Initial sight radius of units. */
+	public static final int INITIAL_SIGHT = 3;
+	/** Health of heart blocks. */
+	public static final int HEART_HEALTH = 100;
+	
+	/** Debug flag. When set to true, core logs to log.txt, otherwise it logs to System.out. */
+	public static final boolean DEBUG_TO_FILE = true;
+	/**
+	 * Level of debugging messages that should be shown.
+	 * -1 = no messages
+	 * 0 = all messages
+	 * 1 = warnings and errors
+	 * 2 = errors
+	 */
+	public static final int DEBUG_LEVEL = 0; 
+}
---
+++ util/run.sh
@@ -0,0 +1,1 @@
+java -cp src net.quadratum.test.MainTest
