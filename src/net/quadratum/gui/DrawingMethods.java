c5dab4b499d27fe9a9dbc9383f2e7a15f68778fe
Nick Gaya <ngaya@cs.brown.edu>
2011-05-05 17:10:29 -0400

    Little update

--- src/net/quadratum/gui/DrawingMethods.java
+++ src/net/quadratum/gui/DrawingMethods.java
@@ -116,7 +116,7 @@ public class DrawingMethods {
 			case RANGE:
 				return Color.ORANGE;
 			case DEFENSE:
-				return new Color(191, 191, 0)z;
+				return new Color(191, 191, 0);
 			case MOVEMENT:
 				return Color.CYAN;
 			case SIGHT:
