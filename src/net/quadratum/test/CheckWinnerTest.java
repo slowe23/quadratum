f95932831c47cc3a77c35f283841695399b21ca8
Nick Gaya <ngaya@cs.brown.edu>
2011-05-06 03:20:11 -0400

    Modifying some stuff and adding map editor

---
+++ src/net/quadratum/test/CheckWinnerTest.java
@@ -0,0 +1,28 @@
+package net.quadratum.test;
+
+import java.util.Map;
+
+import net.quadratum.core.MapPoint;
+import net.quadratum.core.PlayerInformation;
+import net.quadratum.core.Unit;
+import net.quadratum.core.WinCondition;
+
+public class CheckWinnerTest implements WinCondition
+{
+	public boolean hasPlayerWon(Map<MapPoint, Unit> units, PlayerInformation playerInformation)
+	{
+		return false;
+	}
+	
+    public boolean hasPlayerLost(Map<MapPoint, Unit> units, PlayerInformation playerInformation)
+	{
+		if(units.size() == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
