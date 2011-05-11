604452e1902f424cef654fbe38394acaf6cd2cd0
Nick Gaya <ngaya@cs.brown.edu>
2011-05-11 16:07:38 -0400

    Fixings

---
+++ src/net/quadratum/ai/LevelAI.java
@@ -0,0 +1,95 @@
+package net.quadratum.ai;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+import net.quadratum.core.*;
+
+/**
+ * A class that defines a simple, general level AI
+ *
+ * Units are controlled with "behaviors" which are set in the createUnits method
+ */
+public abstract class LevelAI implements Player {
+	protected Core _core;
+	
+	protected int[][] _terrain;
+	
+	private Map<Integer, UnitBehavior> _behaviors;
+
+	private Map<MapPoint, Unit> _units;
+	private boolean _unitsChanged;
+	private final Object _unitsLock;
+	
+	public LevelAI() {
+		_unitsLock = new Object();
+	}
+	
+	public final void start(Core core, MapData mapData, int id, int otherPlayers) {
+		_core = core;
+		_terrain = mapData._terrain;
+		
+		_behaviors = new HashMap<Integer, UnitBehavior>();
+		
+		createUnits(id);
+		
+		_core.ready(this);
+	}
+	
+	protected final void registerUnit(int unitID, UnitBehavior behavior) {
+		_behaviors.put(unitID, behavior);
+	}
+	
+	public abstract void createUnits(int id);
+	
+	public final void updatePieces(List<Piece> pieces) {}
+	
+	public final void end(GameStats stats) {}
+	
+	public final void lost() {}
+	
+	public final void turnStart() {
+		boolean refresh;
+		
+		Map<MapPoint, Unit> units;
+		do {
+			synchronized(_unitsLock) {
+				units = _units;
+				refresh = _unitsChanged = false;;
+			}
+			for(Iterator<Entry<MapPoint, Unit>> iterator = units.entrySet().iterator(); !refresh && iterator.hasNext(); ) {
+				Entry<MapPoint, Unit> entry = iterator.next();
+				int unit = entry.getValue()._id;
+				UnitBehavior unitBehavior = _behaviors.get(unit);
+				if(unitBehavior!=null) {
+					MapPoint act = unitBehavior.behave(entry.getKey(), _core.getValidActions(this, unit), units);
+					if(act!=null)
+						_core.unitAction(this, unit, act);
+				}
+				synchronized(_unitsLock) {
+					refresh = _unitsChanged;
+				}
+			}
+		} while(refresh);
+		
+		_core.endTurn(this);
+	}
+	
+	public final void updateMapData(MapData mapData) { }
+	
+	public final void updateMap(Map<MapPoint,Integer> units, Set<MapPoint> sight, Action lastAction) {
+		synchronized(_unitsLock) {
+			_units = new HashMap<MapPoint, Unit>();
+			for(Entry<MapPoint, Integer> entry : units.entrySet()) {
+				Unit u = _core.getUnit(this, entry.getValue());
+				if(u!=null)
+					_units.put(entry.getKey(), u);
+			}
+			_unitsChanged = true;
+		}
+	}
+	
+	public final void chatMessage(int from, String message) { }
+	
+	public final void updateTurn(int id) { }
+}
---
+++ src/net/quadratum/test/MainTest2.java
@@ -0,0 +1,49 @@
+package net.quadratum.test;
+
+import java.util.ArrayList;
+
+import net.quadratum.ai.test.TestAI_MTC;
+import net.quadratum.core.Block;
+import net.quadratum.core.GameCore;
+import net.quadratum.core.MapPoint;
+import net.quadratum.core.Piece;
+import net.quadratum.core.Player;
+import net.quadratum.gui.GUIPlayer;
+import net.quadratum.main.CheckWinner;
+import net.quadratum.main.Main;
+
+public class MainTest2 implements Main {
+	public static void main(String[] args)
+	{
+		Player player1 = new TestAI_MTC();
+		Player player2 = new GUIPlayer();
+		// piece
+		ArrayList<Piece> pieces = new ArrayList<Piece>();
+		Block awesomeBlock = new Block(30);
+		awesomeBlock._bonuses.put(Block.BonusType.ATTACK, 100);
+		awesomeBlock._bonuses.put(Block.BonusType.RANGE, 100);
+		awesomeBlock._bonuses.put(Block.BonusType.DEFENSE, 100);
+		awesomeBlock._bonuses.put(Block.BonusType.SIGHT, 100);
+		awesomeBlock._bonuses.put(Block.BonusType.MOVEMENT, 100);
+		awesomeBlock._bonuses.put(Block.BonusType.WATER_MOVEMENT, 100);
+		Piece awesomePiece = new Piece(10, "Awesome Piece", "Provides awesome");
+		awesomePiece.addBlock(new MapPoint(0, 0), new Block(awesomeBlock));
+		awesomePiece.addBlock(new MapPoint(0, 1), new Block(awesomeBlock));
+		awesomePiece.addBlock(new MapPoint(1, 0), new Block(awesomeBlock));
+		awesomePiece.addBlock(new MapPoint(1, 1), new Block(awesomeBlock));
+		pieces.add(awesomePiece);
+		// core
+		GameCore core = new GameCore(new MainTest2(),
+				"maps/smalltest.qmap",
+				new CheckWinner(), pieces);
+		core.addPlayer(player1, "Test Player 1", 5, 100);
+		core.addPlayer(player2, "Test Player 2", 5, 100);
+		core.startGame();
+	}
+
+	@Override
+	public void returnControl() {
+		System.out.println("Done.");
+		System.exit(0);
+	}
+}
